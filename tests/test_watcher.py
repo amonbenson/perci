76e028fad8a50d59b75d42b58ded52e0c5134f9e
Amon Benson <a.benson@komische-oper-berlin.de>
2024-09-16 22:00:32 +0200

    added more watch tests

--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -5,7 +5,7 @@ from src.perci import reactive, watch
 from src.perci.changes import Change, AddChange, RemoveChange, UpdateChange
 
 
-def test_root_watcher():
+def test_add_keys():
     state = reactive(
         {
             "name": "Alice",
@@ -14,8 +14,7 @@ def test_root_watcher():
     )
 
     handler = Mock()
-
-    watcher = watch(state, handler)
+    watch(state, handler)
 
     state["name"] = "Bob"
     handler.assert_called_once_with(UpdateChange(path=["root", "name"], value="Bob"))
@@ -34,3 +33,42 @@ def test_root_watcher():
             call(UpdateChange(path=["root", "address", "state"], value="NY")),
         ]
     )
+    handler.reset_mock()
+
+
+def test_update_keys():
+    state = reactive(
+        {
+            "name": "Alice",
+            "age": 25,
+        }
+    )
+
+    handler = Mock()
+    watch(state, handler)
+
+    state["age"] = {
+        "years": 25,
+        "months": 6,
+    }
+    handler.assert_has_calls(
+        [
+            call(RemoveChange(path=["root"], key="age")),
+            call(AddChange(path=["root"], key="age")),
+            call(AddChange(path=["root", "age"], key="years")),
+            call(UpdateChange(path=["root", "age", "years"], value=25)),
+            call(AddChange(path=["root", "age"], key="months")),
+            call(UpdateChange(path=["root", "age", "months"], value=6)),
+        ]
+    )
+    handler.reset_mock()
+
+    state["age"] = 26
+    handler.assert_has_calls(
+        [
+            call(RemoveChange(path=["root"], key="age")),
+            call(AddChange(path=["root"], key="age")),
+            call(UpdateChange(path=["root", "age"], value=26)),
+        ]
+    )
+    handler.reset_mock()
